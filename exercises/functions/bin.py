33a611c88adced2da6ad092e0b816717d7853636
Guilherme Araújo de Oliveira <garaujodeoliveira1@gmail.com>
2021-05-24 17:41:47 -0300

    Update bin.py

--- exercises/functions/bin.py
+++ exercises/functions/bin.py
@@ -3,13 +3,6 @@ def binario(num):
   return print(bin(num))
 
 
-n1 = input()
-n2 = input()
-n3 = input()
-n4 = input()
-n5 = input()
-binario(n1)
-binario(n2)
-binario(n3)
-binario(n4)
-binario(n5)
+numbers = [x for x in input().split()]
+for n in numbers:
+  binario(n)
